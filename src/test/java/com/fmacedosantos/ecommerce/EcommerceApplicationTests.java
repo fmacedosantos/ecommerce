a7e68cdb7fe896b7edef31565250bf1b0a9fdddd
fmacedosantos <felipemacedo986@gmail.com>
2025-05-11 15:38:12 -0300

    Project created

---
+++ src/test/java/com/fmacedosantos/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmacedosantos.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
