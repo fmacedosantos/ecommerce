99ecd94ba68381f29770c8d4600b0a43721fe160
fmacedosantos <felipemacedo986@gmail.com>
2025-05-11 19:20:10 -0300

    OrderStatus enum

--- src/main/java/com/fmacedosantos/ecommerce/config/TestConfig.java
+++ src/main/java/com/fmacedosantos/ecommerce/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.fmacedosantos.ecommerce.config;
 
 import com.fmacedosantos.ecommerce.entities.Order;
 import com.fmacedosantos.ecommerce.entities.User;
+import com.fmacedosantos.ecommerce.entities.enums.OrderStatus;
 import com.fmacedosantos.ecommerce.repositories.OrderRepository;
 import com.fmacedosantos.ecommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); // Instant no formato ISO 8601
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1); // Instant no formato ISO 8601
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/com/fmacedosantos/ecommerce/entities/Order.java
+++ src/main/java/com/fmacedosantos/ecommerce/entities/Order.java
@@ -1,6 +1,7 @@
 package com.fmacedosantos.ecommerce.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fmacedosantos.ecommerce.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,6 +18,7 @@ public class Order implements Serializable {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT") // Definindo o retorno em Json para corresponder à ISO 8601
     private Instant moment;
+    private Integer orderStatus;
 
     @ManyToOne // Configura o tipo de relação de Order com User em vista da classe Order
     @JoinColumn(name = "client_id") // Configura a chave estrangeira
@@ -25,9 +27,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -53,6 +56,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
